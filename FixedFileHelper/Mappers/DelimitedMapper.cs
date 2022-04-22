7c70f5dd7dd753e10eee6c27bbd08b60fcc9a104
hans.voll <hans.voll@mrsbpo.com>
2022-04-22 14:58:40 -0400

    Add project

---
+++ FixedFileHelper/Mappers/DelimitedMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileHelper.Mappers
+{
+    public static class DelimitedMapper
+    {
+        public static string GetContents(string input)
+        {
+            return "library is getting the contents you need...";
+        }
+    }
+}
